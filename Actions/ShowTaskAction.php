bc71b78e346839f7c05d44b9dc40b3e3667114d0
bgaheer <baljeetgaheer@gmail.com>
2018-11-26 17:19:05 +0530

    changes

---
+++ Actions/ShowTaskAction.php
@@ -0,0 +1,20 @@
+<?php
+require_once('../IConstants.inc');
+require_once($ConstantsArray['dbServerUrl'] ."Managers/ShowTaskMgr.php");
+require_once($ConstantsArray['dbServerUrl'] ."BusinessObjects/ShowTask.php");
+$success = 1;
+$call = "";
+$response = new ArrayObject();
+if(isset($_GET["call"])){
+	$call = $_GET["call"];
+}else{
+	$call = $_POST["call"];
+}
+$showTaskMgr = ShowTaskMgr::getInstance();
+if($call == "getShowTasks"){
+	$showSeq = $_GET["showSeq"];
+	$taskJson = $showTaskMgr->getTaskByShowSeq($showSeq);
+	$response["tasks"]= $taskJson;
+	echo json_encode($response);
+	return;
+}
