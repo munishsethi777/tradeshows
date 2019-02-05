1c24b93e7a57031fa075d942c6297ac69062e431
bgaheer <baljeetgaheer@gmail.com>
2019-02-05 12:57:47 +0530

    changes

--- database/create_Customers.sql
+++ database/create_Customers.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.8.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Feb 01, 2019 at 03:47 PM
--- Server version: 10.1.36-MariaDB
--- PHP Version: 5.6.38
+-- Generation Time: Feb 05, 2019 at 12:56 PM
+-- Server version: 10.1.34-MariaDB
+-- PHP Version: 5.6.37
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -36,7 +36,7 @@ CREATE TABLE `customers` (
   `address` varchar(250) DEFAULT NULL,
   `address1` varchar(250) DEFAULT NULL,
   `city` varchar(50) DEFAULT NULL,
-  `street` varchar(100) DEFAULT NULL,
+  `state` varchar(100) DEFAULT NULL,
   `zip` varchar(10) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
   `attention` varchar(100) DEFAULT NULL,
@@ -46,10 +46,31 @@ CREATE TABLE `customers` (
   `sales2` varchar(100) DEFAULT NULL,
   `sales3` varchar(100) DEFAULT NULL,
   `sales4` varchar(100) DEFAULT NULL,
-  `createdate` datetime DEFAULT NULL,
+  `createdate` date DEFAULT NULL,
   `createdon` datetime NOT NULL,
   `lastmodifiedon` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`seq`),
+  ADD UNIQUE KEY `customerid` (`customerid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `seq` bigint(20) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
