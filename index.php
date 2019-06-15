69454d547f17b54c182e6c5fbfb7e089fd7eb4f5
bgaheer <baljeetgaheer@gmail.com>
2019-06-15 11:45:48 +0530

    ca

--- index.php
+++ index.php
@@ -1,28 +1,4 @@
 <?php 
-require_once('IConstants.inc');
-//require_once($ConstantsArray['dbServerUrl'] ."Utils/MailUtil.php");
-//MailUtil::sendSmtpMail("test subject","body","munishsethi777@gmail.com");
+require_once('userlogin.php');?>
 
-?>
-<html>
-<head>
-<title>Booking</title>
-	<?include "ScriptsInclude.php"?>
-	
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body class="gray-bg">
- <div class="middle-box text-center loginscreen animated fadeInDown">
-        <div>
-        	<img src="images/logo.png" style="margin-bottom:20px;">
-            <h3 style="line-height: 20px">Welcome to TradeShows Management Application</h3>
-            <br><br>
-            <a class="btn btn-primary" href="userlogin.php">Admin Login</a>
-            <a class="btn btn-info" href="userlogin.php">User Login</a>
-            <p class="m-t"> <small>This is a demonstration application and some parts of the application may have static
-            data/information and only few menu items are functional.</small> </p>
-        </div>
-    </div>
-</body>
-</html>
  
