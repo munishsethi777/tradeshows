4271cfb5cf7b9ab284f06cc29680a4b7996e88be
bgaheer <baljeetgaheer@gmail.com>
2019-06-15 11:50:05 +0530

    cc

--- userlogin.php
+++ userlogin.php
@@ -100,7 +100,7 @@ require_once('IConstants.inc');
                     </div>
                 </form>
             
-            <a class="btn btn-default btn-sm" href="index.php">HOME</a>
+<!--             <a class="btn btn-default btn-sm" href="index.php">HOME</a> -->
             <p class="m-t"> <small>Login Credentials/Rights Reserved</small> </p>
         </div>
     </div>
