5b8d8a4e7b460059f52e93163ac1e8cbcd310599
bgaheer <baljeetgaheer@gmail.com>
2019-07-05 11:56:36 +0530

    c

---
+++ Enums/Permissions.php
@@ -0,0 +1,8 @@
+<?php
+require_once($ConstantsArray['dbServerUrl'] ."Enums/BasicEnum.php");
+class Permissions extends BasicEnum{
+	const qc = "QC";
+	const usa_team = "USA Team";
+	const china_team = "China Team";
+	const graphic_designer = "Graphic Designer";
+}
---
+++ GraphicLogsNotesUpdatedTemplate.php
@@ -0,0 +1,8 @@
+<div>
+	<p>Hi,<br>
+	<p>{FULL_NAME},<p>
+	<p>{NOTES_NAME} notes updated by {LOGGED_IN_USER_NAME}</p>
+	<p>Notes :- {NOTES_DETAIL}</p>
+	<br>
+	<p>Thanks<br>
+</div>
