0cb94614a61de0d4691be3ed20f4478220751cf9
bgaheer <baljeetgaheer@gmail.com>
2019-07-08 10:05:02 +0530

    c

--- StringConstants.php
+++ StringConstants.php
@@ -3,6 +3,7 @@ class StringConstants{
 	const UPCOMING_INSPECTION_SCHEDULE = "ALPINE BI | Upcoming Inspection Schedules";
 	const UPCOMING_INSPECTION_APPOITMENT = "ALPINE BI | Upcoming Inspection Appointments";
 	const MISSING_INSPECTION_APPOINTMENT = "ALPINE BI | Missing Inspection Appointments";
+	const PENDING_QC_APPROVALS = "ALPINE BI | Pending QC Approvals";
 	const INCOMPLETED_SCHEDULES = "ALPINE BI | Incompleted Schedules";
 	
 	const WEB_PORTAL_LINK = "http://www.alpinebi.com";
