c6bf39887d391f1ceb057af30aae47b7dd1c5175
bgaheer <baljeetgaheer@gmail.com>
2019-07-08 09:21:35 +0530

    c

--- GraphicLogsNotesUpdatedTemplate.php
+++ GraphicLogsNotesUpdatedTemplate.php
@@ -1,8 +1,7 @@
 <div>
-	<p>Hi,<br>
-	<p>{FULL_NAME},<p>
-	<p>{NOTES_NAME} notes updated by {LOGGED_IN_USER_NAME}</p>
-	<p>Notes :- {NOTES_DETAIL}</p>
+	<p>Hi <b>{FULL_NAME}</b>,<br>
+	<p>Following are the {NOTES_NAME} notes updated by {LOGGED_IN_USER_NAME} for <b>{PO_NUMBER}</b> and item <b>{ITEM_ID}</b></p>
+	<p style="font-style: italic;">{NOTES_DETAIL}</p>
 	<br>
 	<p>Thanks<br>
 </div>
