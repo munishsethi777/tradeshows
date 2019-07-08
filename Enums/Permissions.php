5a0dfe548a3cb47d7fe90b7ea3f3a770b1f8aac7
bgaheer <baljeetgaheer@gmail.com>
2019-07-08 10:05:51 +0530

    c

--- Enums/Permissions.php
+++ Enums/Permissions.php
@@ -5,4 +5,8 @@ class Permissions extends BasicEnum{
 	const usa_team = "USA Team";
 	const china_team = "China Team";
 	const graphic_designer = "Graphic Designer";
+	const container_information = "Container Information";
+	const container_delivery_information = "Container Delivery Information";
+	const container_office_information = "Container Office Information";
+	const class_code = "Class Code";
 }
