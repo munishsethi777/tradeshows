205db856fd55a43872513ab5b78e6e62c78c2e03
bgaheer <baljeetgaheer@gmail.com>
2019-08-22 10:47:02 +0530

    c

--- adminManageQCSchedules.php
+++ adminManageQCSchedules.php
@@ -211,8 +211,8 @@ $(document).ready(function(){
 	$("#updateApprovalStatusBtn").click(function(e){
 		updateApprovalStatus()
 	});
-   	loadGrid();
-   	initDateRanges();
+	loadGrid();
+	initDateRanges();
    	$('.i-checks').iCheck({
 		checkboxClass: 'icheckbox_square-green',
 	   	radioClass: 'iradio_square-green',
