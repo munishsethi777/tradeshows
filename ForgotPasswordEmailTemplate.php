968ea063832b36bbb0f3132084c1c4dd0c8c4b24
bgaheer <baljeetgaheer@gmail.com>
2019-08-23 09:38:20 +0530

    c

---
+++ ForgotPasswordEmailTemplate.php
@@ -0,0 +1,8 @@
+<div>
+<p>Hi,<br>
+	You have asked to reset your password in Alpinebi.com.
+</p>
+<p>In order to get a new password, please <a href="{FOGOT_PASSWORD_LINK}">click this link.</a></p>
+<br>
+<p>Regards,<br>Alpinebi
+</div>
