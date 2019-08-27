a73616322d055d56158e5d536bdaf843c25f5c71
bgaheer <baljeetgaheer@gmail.com>
2019-08-27 15:34:31 +0530

    c

---
+++ QCApprovedRjectEmailTemplate.php
@@ -0,0 +1,6 @@
+<div>
+	<p>Hi,<br>
+	<p>QC {SUPERVISOR_NAME} has submitted a request for your approval on Item {ITEM_ID} in PO. {PO_NUMBER} on dated {DATE_STR}.Please Login into webportal <a href='{WEB_PORTAL_LINK}'>{WEB_PORTAL_LINK}</a> to approve or reject the submission.</p>
+	<br>
+	<p>Regards,<br>Alpinebi
+</div>
