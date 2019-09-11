2cd5e1a5029f3dac5d39a58fe9a55b57df633c59
munishsethi777 <munishsethi777@gmail.com>
2019-09-11 17:38:17 +0530

    email template design

---
+++ emailTemplateContainer.php
@@ -0,0 +1,134 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css"
+	href="1https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+</head>
+<body>
+	<div
+		style="background-color: #f3f3f4; width: 100%; color: #676a6c; font-family: open sans, Helvetica Neue, Helvetica, Arial, sans-serif">
+		<div
+			style="background-color: white; margin: auto; max-width: 800px; padding: 0px 15px 0px 15px">
+			<div style="min-height: 700px;">
+				<div
+					style="padding: 10px; background-color: rgba(26, 178, 147, 0.3) !important; color: white; margin: 0px -15px 0px -15px;">
+	
+					<img src="http://www.alpinebi.com/images/logo.png"
+						style="align: left"> <a href="http://www.alpinebi.com/"
+						style="float: right; color: grey; text-decoration: none;">www.AlpineBI.com</a>
+				</div>
+				<div
+					style="background: white; margin: 0px; padding: 15px; display: flex">
+					<p style="margin: 0px;">
+						Hi,<br> <br> Approval request for <b>PO: 28464-S</b> and <b>Item:
+							QEL748A</b> has been marked <b>'Approved'</b> by May Lariosa
+					</p>
+				</div>
+				
+				<div
+					style="margin: 0; background: #f1f1f1; padding: 20px 20; display: flex">
+					
+					<table width="100%" style="color:grey;font-family:arial;font-weight:normal;font-size:12px;border:1px silver solid" cellpadding="3px" cellspacing="0px">
+						<tbody><tr style="background-color:silver;color:white">
+							<th style="border:1px silver solid;width:5%;text-align:right">Sr.</th>
+							<th style="border:1px silver solid;width:10%;text-align:right">QC</th>
+							<th style="border:1px silver solid;width:10%;text-align:right">Class</th>
+							<th style="border:1px silver solid;width:10%;text-align:right">PO</th>
+							<th style="border:1px silver solid;width:10%;text-align:right">PO Type</th>
+							<th style="border:1px silver solid;width:10%;text-align:right">Item Number</th>
+							<th style="border:1px silver solid;width:10%;text-align:right">ShipDate</th>
+							<th style="border:1px silver solid;width:10%;text-align:right">Scheduled Final Inspection Date</th>
+							<th style="border:1px silver solid;width:10%;text-align:right">Appointment Final Inspection Date</th>
+						</tr>
+						<tr>
+						<td style="border:1px silver solid;width:5%;text-align:right">1</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">JONES</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">WIN </td>
+						<td style="border:1px silver solid;width:10%;text-align:right">28564</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">OPEN PO</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">WIN454<br>WIN322</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">9/1/19</td>
+						<td style="border:1px silver solid;width:10%;text-align:right;color:red">8/22/19</td>
+						<td style="border:1px silver solid;width:10%;text-align:right;color:green">8/30/19</td>
+					</tr><tr>
+						<td style="border:1px silver solid;width:5%;text-align:right">2</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">JONES</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">WIN </td>
+						<td style="border:1px silver solid;width:10%;text-align:right">28327-BP</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">WAREHOUSE</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">WIN220</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">9/1/19</td>
+						<td style="border:1px silver solid;width:10%;text-align:right;color:red">8/22/19</td>
+						<td style="border:1px silver solid;width:10%;text-align:right;color:green">8/31/19</td>
+					</tr><tr>
+						<td style="border:1px silver solid;width:5%;text-align:right">3</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">JOY</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">TZL</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">28199</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">OPEN PO</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">TZL284</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">11/10/19</td>
+						<td style="border:1px silver solid;width:10%;text-align:right;color:red">10/31/19</td>
+						<td style="border:1px silver solid;width:10%;text-align:right;color:green">8/29/19</td>
+					</tr><tr>
+						<td style="border:1px silver solid;width:5%;text-align:right">4</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">JOY</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">ZTY</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">28678</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">OPEN PO</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">ZTY104CC</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">9/15/19</td>
+						<td style="border:1px silver solid;width:10%;text-align:right;color:red">9/5/19</td>
+						<td style="border:1px silver solid;width:10%;text-align:right;color:green">9/9/19</td>
+					</tr><tr>
+						<td style="border:1px silver solid;width:5%;text-align:right">5</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">NIKO</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">QMC</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">28103</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">OPEN PO</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">QMC250SLR</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">9/1/19</td>
+						<td style="border:1px silver solid;width:10%;text-align:right;color:red">8/22/19</td>
+						<td style="border:1px silver solid;width:10%;text-align:right;color:green">9/6/19</td>
+					</tr><tr>
+						<td style="border:1px silver solid;width:5%;text-align:right">6</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">OWEN</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">QEL</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">28410</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">OPEN PO</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">QEL738A</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">10/13/19</td>
+						<td style="border:1px silver solid;width:10%;text-align:right;color:red">10/3/19</td>
+						<td style="border:1px silver solid;width:10%;text-align:right;color:green">9/3/19</td>
+					</tr><tr>
+						<td style="border:1px silver solid;width:5%;text-align:right">7</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">TERRY</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">QLP</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">28108</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">OPEN PO</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">QLP1352SLR-HH-TM</td>
+						<td style="border:1px silver solid;width:10%;text-align:right">9/1/19</td>
+						<td style="border:1px silver solid;width:10%;text-align:right;color:red">8/22/19</td>
+						<td style="border:1px silver solid;width:10%;text-align:right;color:green">8/31/19</td>
+					</tr>
+					</tbody></table>
+					
+					
+				</div>
+
+			</div>
+
+
+			<p style="background: #676a6c; margin: 0px -15px 0px -15px; color: #FFF; padding:10px;font-size:12px"	align="center">
+				This is an automated email and can not be replied. For any further assistance, mail to munishsethi@satyainfopages.com</br>
+				You may login at http://www.alpinebi.com to access Alpine Business Intelligence online application<br>
+				© 2019 Alpine Corporation.
+			</p>
+
+
+
+
+		</div>
+
+	</div>
+</body>
+</html>
