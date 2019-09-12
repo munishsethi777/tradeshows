a86b48c0e35d31417ca3a81a2b229bdfb72e93df
bgaheer <baljeetgaheer@gmail.com>
2019-09-12 14:41:21 +0530

    c

--- emailTemplate.php
+++ emailTemplate.php
@@ -1,14 +1,16 @@
-<table width="100%" style="color:grey;font-family:arial;font-weight:normal;font-size:12px;border:1px silver solid" cellpadding="3px" cellspacing="0px">
-	<tr style="background-color: silver;color:white">
-		<th style="border:1px silver solid;width:5%;text-align:right">Sr.</th>
-		<th style="border:1px silver solid;width:10%;text-align:right">QC</th>
-		<th style="border:1px silver solid;width:10%;text-align:right" >Class</th>
-		<th style="border:1px silver solid;width:10%;text-align:right">PO</th>
-		<th style="border:1px silver solid;width:10%;text-align:right">PO Type</th>
-		<th style="border:1px silver solid;width:10%;text-align:right">Item Number</th>
-		<th style="border:1px silver solid;width:10%;text-align:right">ShipDate</th>
-		<th style="border:1px silver solid;width:10%;text-align:right">{NOTIFICATION_DATE_TITLE}</th>
-		<th style="border:1px silver solid;width:10%;text-align:right">{AP_NOTIFICATIONDATE_TITLE}</th>
-	</tr>
-	{TABLE_ROWS}
-</table>
+<div style="margin: 0; background: #f1f1f1; padding: 20px 20; display: flex">
+    <table width="100%" style="color:grey;font-family:arial;font-weight:normal;font-size:12px;border:1px silver solid" cellpadding="3px" cellspacing="0px">
+    	<tr style="background-color: silver;color:white">
+    		<th style="border:1px silver solid;width:5%;text-align:right">Sr.</th>
+    		<th style="border:1px silver solid;width:10%;text-align:right">QC</th>
+    		<th style="border:1px silver solid;width:10%;text-align:right" >Class</th>
+    		<th style="border:1px silver solid;width:10%;text-align:right">PO</th>
+    		<th style="border:1px silver solid;width:10%;text-align:right">PO Type</th>
+    		<th style="border:1px silver solid;width:10%;text-align:right">Item Number</th>
+    		<th style="border:1px silver solid;width:10%;text-align:right">ShipDate</th>
+    		<th style="border:1px silver solid;width:10%;text-align:right">{NOTIFICATION_DATE_TITLE}</th>
+    		<th style="border:1px silver solid;width:10%;text-align:right">{AP_NOTIFICATIONDATE_TITLE}</th>
+    	</tr>
+    	{TABLE_ROWS}
+    </table>
+</div>
