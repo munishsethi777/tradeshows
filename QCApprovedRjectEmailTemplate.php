14c9dbbf4c2fcccc281e6856dc9e211063691732
bgaheer <baljeetgaheer@gmail.com>
2019-09-18 10:51:45 +0530

    c

--- QCApprovedRjectEmailTemplate.php
+++ QCApprovedRjectEmailTemplate.php
@@ -1,6 +1,6 @@
 <div>
 	<p>Hi,<br>
-		Approval request for PO: {PO_NUMBER} Item: {ITEM_NUMBER} has been {APPROVED_REJECT_STATUS} by {SUPERVISOR_NAME}</p>
+		Approval request for PO: {PO_NUMBER} Item: {ITEM_NUMBER} is marked as <b>{APPROVED_REJECT_STATUS}</b> by {SUPERVISOR_NAME}</p>
 	<br>
 	Comments : {RESPONSE_COMMENTS}
 	<p>Regards,<br>Alpinebi
