ef436cf17ba1543625d2a895822c298e43f090c4
bgaheer <baljeetgaheer@gmail.com>
2020-01-07 17:08:48 +0530

    c

---
+++ Enums/QCScheduleNotificationType.php
@@ -0,0 +1,10 @@
+<?php
+require_once($ConstantsArray['dbServerUrl'] ."Enums/BasicEnum.php");
+class QCScheduleNotificationType extends BasicEnum{
+    const upcoming_inspections_report_weekly = "Upcoming Inspections Report (Weekly)";
+    const missing_appointments_report_weekly = "Missing Appointments Report (Weekly)";
+    const incompleted_schedules_report_weekly = "Incompleted Schedules Report (Weekly)";
+    const pending_qc_approval_report_weekly = "Pending QC Approval Report (Weekly)";
+    const qc_planner_report_weekly = "QC Planner Report (Weelky)";
+    const qc_approved_rejected_instant = "QC Approved/Rejected (Instant)";
+}
