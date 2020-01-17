5b9a97e39b366c04693621cd8a9040f84b071d40
munishsethi777 <munishsethi777@gmail.com>
2020-01-17 16:44:41 +0530

    domestic spellings

--- Enums/CustomerBusinessType.php
+++ Enums/CustomerBusinessType.php
@@ -2,6 +2,6 @@
 require_once($ConstantsArray['dbServerUrl'] ."Enums/BasicEnum.php");
 class CustomerBusinessType extends BasicEnum{
     const direct = "Direct";
-    const domesitc = "Domesitc";
+    const domestic = "Domestic";
     const dot_com = "DotCom";
 }
