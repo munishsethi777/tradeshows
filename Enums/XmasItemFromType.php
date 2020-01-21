73f3cbc44466644346ba78b6de1af3ce112b787a
bgaheer <baljeetgaheer@gmail.com>
2020-01-21 12:55:51 +0530

    c

---
+++ Enums/XmasItemFromType.php
@@ -0,0 +1,8 @@
+<?php
+require_once($ConstantsArray['dbServerUrl'] ."Enums/BasicEnum.php");
+class XmasItemFromType extends BasicEnum{
+    const meet_from_catelog = "Will Select from Catalog";
+    const need_to_make_presentation = "Need to make Presentation";
+    const coming_to_our_showroom = "Coming to our Showroom";
+    const meet_in_atlanta = "Meeting in Atlanta";
+}
