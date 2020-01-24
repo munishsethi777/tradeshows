aab94075aef9ab05a31d38e46f3687c20a0908ac
munishsethi777 <munishsethi777@gmail.com>
2020-01-24 13:59:21 +0530

    email template

--- emailTemplateContainer.php
+++ emailTemplateContainer.php
@@ -12,7 +12,108 @@
 						style="float: right; color: grey; text-decoration: none;">www.AlpineBI.com</a></div>
 				</div>
 				 {EMAIL_CONTENT}
-				 
+				<table border="0" cellpadding="0" width="100%" style="color:#676a6c;width: 100.0%; border-spacing: 0; font-size:13px;margin:10px 0px 10px 0px">
+					<thead>
+						<tr>
+							<td style="padding: .75pt .75pt .75pt .75pt;text-align:left">
+								<b>Container</b>
+							</td>
+							<td style="padding: .75pt .75pt .75pt .75pt;text-align: center">
+								<b>Samples Received</b>
+							</td>
+							<td style="padding: .75pt .75pt .75pt .75pt;text-align: center">
+								<b>Container in OMS</b>
+							</td>
+							<td style="padding: .75pt .75pt .75pt .75pt;text-align: center">
+								<b>Samples in OMS</b>
+							</td>
+							<td style="padding: .75pt .75pt .75pt .75pt;text-align: center">
+								<b>Container in WMS</b>
+							</td>
+							<td style="padding: .75pt .75pt .75pt .75pt;text-align: center">
+								<b>Samples in WMS</b>
+							</td>
+						</tr>
+					</thead>
+					<tbody>
+						<tr style="background-color: #f3f3f4">
+							<td style="padding:8px 4px;text-align:left">
+								<div style="color:#0093bf;font-size:15px;">#12324231</div>
+								<div style="margin-top:5px;">
+									ETA  01/11/20 01:00 am<br>
+									MSRF 01/11/20 01:00 am
+								</div>
+							</td>
+							<td style="padding:8px 4px;text-align: center">
+								01/11/2020
+							</td>
+							<td style="padding:8px 4px;text-align: center">
+								01/11/2020
+							</td>
+							<td style="padding:8px 4px;text-align: center">
+								-
+							</td>
+							<td style="padding:8px 4px;text-align: center">
+								-
+							</td>
+							<td style="padding:8px 4px;text-align: center">
+								01/11/2020
+							</td>
+						</tr>
+						<tr style="background-color: #ffffff">
+							<td style="padding:8px 4px;text-align:left">
+								<div style="color:#0093bf;font-size:15px;">#12324231</div>
+								<div style="margin-top:5px;">
+									ETA  01/11/20 01:00 am<br>
+									MSRF 01/11/20 01:00 am
+								</div>
+							</td>
+							<td style="padding:8px 4px;text-align: center">
+								01/11/2020
+							</td>
+							<td style="padding:8px 4px;text-align: center">
+								01/11/2020
+							</td>
+							<td style="padding:8px 4px;text-align: center">
+								-
+							</td>
+							<td style="padding:8px 4px;text-align: center">
+								01/11/2020
+							</td>
+							<td style="padding:8px 4px;text-align: center">
+								-
+							</td>
+						</tr>
+						<tr style="background-color: #f3f3f4">
+							<td style="padding:8px 4px;text-align:left">
+								<div style="color:#0093bf;font-size:15px;">#12324231</div>
+								<div style="margin-top:5px;">
+									ETA  01/11/20 01:00 am<br>
+									MSRF 01/11/20 01:00 am
+								</div>
+							</td>
+							<td style="padding:8px 4px;text-align: center">
+								01/11/2020
+							</td>
+							<td style="padding:8px 4px;text-align: center">
+								-
+							</td>
+							<td style="padding:8px 4px;text-align: center">
+								01/11/2020
+							</td>
+							<td style="padding:8px 4px;text-align: center">
+								-
+							</td>
+							<td style="padding:8px 4px;text-align: center">
+								01/11/2020
+							</td>
+						</tr>
+					</tbody>
+				</table>
+				
+
+
+
 			</div>
 			<p style="background: #676a6c; margin: 0px -15px 0px -15px; color: #FFF; padding:10px;font-size:12px"	align="center">
 				This is an automated email and can not be replied.</br>
