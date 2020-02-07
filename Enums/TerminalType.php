18b16d744bad6b6140f34274073f72acfa056562
munishsethi777 <munishsethi777@gmail.com>
2020-02-07 10:27:14 +0530

    terminal type enum added ITS

--- Enums/TerminalType.php
+++ Enums/TerminalType.php
@@ -3,6 +3,7 @@ require_once($ConstantsArray['dbServerUrl'] ."Enums/BasicEnum.php");
 class TerminalType extends BasicEnum{
     const apl = "APL";
     const apm = "APM";
+    const its = "ITS";
     const lbct = "LBCT";
     const pct = "PCT";
     const shippers = "SHIPPERS";
