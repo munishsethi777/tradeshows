a3025527ffcba01e1625a90f5526c0c7b4a3cb43
munishsethi777 <munishsethi777@gmail.com>
2020-05-29 13:39:35 +0530

    final graphic due date changed template

---
+++ emailtemplates/GraphicFinalGraphicDueDateChangedTemplate.php
@@ -0,0 +1,6 @@
+<div>
+	<p>Hi,<br>
+	<p>User {LOGGED_IN_USER_NAME} has changed Final Graphics Due Date for Item {ITEM_ID} in PO. {PO_NUMBER} on dated {DATE_STR}.</p>
+	<br>
+	<p>Regards,<br>Alpinebi
+</div>
