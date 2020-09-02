152b33c35a67a1b5e97b5d40ecc5cc4349bcc74e
munishsethi777 <munishsethi777@gmail.com>
2020-09-02 09:02:23 +0530

    customer - buyer category

--- Enums/BuyerCategoryType.php
+++ Enums/BuyerCategoryType.php
@@ -1,17 +1,30 @@
 <?php
 require_once($ConstantsArray['dbServerUrl'] ."Enums/BasicEnum.php");
 class BuyerCategoryType extends BasicEnum{
-    const fountains = "Fountains";
-    const bird_bath = "Bird Bath";
-    const furniture = "Furniture";
-    const other = "Other";
-    const christmas_lighted = "Christmas Lighted";
-    const outdoor_lighted = "Outdoor Lighted";
+    const birding = "Birding";
     const christmas = "Christmas";
+    const fountains = "Fountains";
+    const garden_furniture = "Garden Furniture";
+    const garden_stakes = "Garden Stakes(Non-Solar)";
+    const gazing_globes = "Gazing Globes";
+    const halloween = "Halloween";
+    const harvest = "Harvest";
     const outdoor_decor = "Outdoor Decor";
     const patriotic = "Patriotic";
-    const birding = "Birding";
-    const lighted_stakes = "lighted_stakes";
-    const all_decor = "All Decor";
-    const indoor_decor = "Indoor Decor";
+    const planters = "Planters";
+    const pond = "Pond";
+    const solar = "Solar";
+    const statuary = "Statuary";
+    
+    
+//     const bird_bath = "Bird Bath";
+//     const furniture = "Furniture";
+//     const other = "Other";
+//     const christmas_lighted = "Christmas Lighted";
+//     const outdoor_lighted = "Outdoor Lighted";
+//     const lighted_stakes = "lighted_stakes";
+//     const all_decor = "All Decor";
+//     const indoor_decor = "Indoor Decor";
+    
+    
 }
