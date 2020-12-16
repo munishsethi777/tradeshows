d6aa69e404040c80422c310ae77e74ba5f69197c
munishsethi777 <munishsethi777@gmail.com>
2020-12-16 11:36:02 +0530

    mails relate chnages

--- emailtemplates/InstructionManualNotesToUsaUpdatedTemplate.php
+++ emailtemplates/InstructionManualNotesToUsaUpdatedTemplate.php
@@ -1,9 +1,12 @@
 <div>
 	<p>Hi<br>
 	<p>An Instruction Manual log has got its {NOTES_NAME} Note updated. Following are the details:</p>
-	<p>ItemNumber: '{ITEM_NUMBER}'</p>
-	<p style="font-style: italic;">{NOTES_DETAIL}</p><br>
-	<p>Updated By: '{LOGGED_IN_USER_NAME}'</p><br>
-	<p>Changed on: '{CURR_DATE_TIME}'</p>
-	<p>Thanks<br>
+	<p>
+	 	ItemNumber: '{ITEM_NUMBER}'<br>
+		Notes to USA: '{NOTES_DETAIL}'<br>
+		Updated By: '{LOGGED_IN_USER_NAME}'<br>
+		Changed on: '{CURR_DATE_TIME}'<br>
+	 </p>
+	<br>
+	<p>Regards,<br>Alpinebi
 </div>
