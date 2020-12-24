af2be400571f9d18d440ea479282a7203aaac875
Mukesh <mukesh.24065@gmail.com>
2020-12-24 16:31:31 +0530

    Customer related enums

--- Enums/CustomerBusinessType.php
+++ Enums/CustomerBusinessType.php
@@ -3,9 +3,10 @@ require_once($ConstantsArray['dbServerUrl'] ."Enums/BasicEnum.php");
 class CustomerBusinessType extends BasicEnum{
     //const direct = "Direct";
     //const domestic = "Domestic/Import";
-    const dot_com = "DotCom";
+    const ecomm = "eComm";
     const coop = "Coop";
     const distributor = "Distributor";
     const grocery = "Grocery";
     const dealer = "Dealer";
+    const regional_mass_merchant = "Regional Mass Merchant";
 }
