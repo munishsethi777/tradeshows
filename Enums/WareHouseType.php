a49f7ddf03bf097721b1534a89c758077e2a60af
munishsethi777 <munishsethi777@gmail.com>
2021-02-12 11:10:05 +0530

    wareHouseType

--- Enums/WareHouseType.php
+++ Enums/WareHouseType.php
@@ -2,6 +2,7 @@
 require_once($ConstantsArray['dbServerUrl'] ."Enums/BasicEnum.php");
 class WareHouseType extends BasicEnum{
     const alpine = "Alpine";
+    const coi = "COI - City of Industry";
     const dcw_m_and_a = "DCW / M&A";
     const dhe = "DHE";
     const direct_to_customer = "Direct To Customer";
