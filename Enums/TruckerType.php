7abf6eeb9bf30b5b07d65e9fef8e111c45caf5d8
munishsethi777 <munishsethi777@gmail.com>
2021-02-16 15:36:44 +0530

    additon of new truckers and warehouses

--- Enums/TruckerType.php
+++ Enums/TruckerType.php
@@ -1,8 +1,19 @@
 <?php
 require_once($ConstantsArray['dbServerUrl'] ."Enums/BasicEnum.php");
 class TruckerType extends BasicEnum{
-    const alpine = "KKG";
+    const amazon_trucking = "Amazon Trucking & Distribution";
+    const brotherhood_transportation = "Brotherhood Transportation";
     const cbl = "CBL";
-    const m_and_a_dcw = "M&A / DCW";
+    const duncan_and_sons = "Duncan and Sons";
+    const eco_express = "Eco Express";
+    const forrest_logistics = "Forrest Logistics LLC";
+    const alpine = "KKG";
     const kkgr = "KKGR";
+    const m_and_a_dcw = "M&A / DCW";
+    const moon_global = "Moon Global";
+    const pac_anchor_trucking = "Pac Anchor Trucking";
+    const rbw_logistics = "RBW Logistics";
+    const realitic_transportation = "Realistic Transportation";
+    const team_alliance = "Team alliance";
+    const uls_group = "ULS United Logistics Services Group";
 }
