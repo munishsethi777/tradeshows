b86315ed8e8eb61fba0fe06dc28709118a1dc0b4
Mukesh Sharma <mukesh.24065@gmail.com>
2021-02-20 13:03:52 +0530

    xmasitemsfromtype

---
+++ XmasItemFromType.php
@@ -0,0 +1,8 @@
+<?php
+require_once($ConstantsArray['dbServerUrl'] ."Enums/BasicEnum.php");
+class XmasItemFromType extends BasicEnum{
+    const meet_from_catelog = "Will Select from Catalog";
+    const need_to_make_presentation = "Need to make Presentation";
+    const alpine_showroom_visit = "Alpine Showroom Visit";
+    const meet_in_atlanta = "Meeting in Atlanta";
+}
