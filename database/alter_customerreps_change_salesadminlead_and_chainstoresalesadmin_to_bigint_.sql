683f7f78db75dfda5ac75ccbda37ff0db8b6992c
Mukesh Sharma <mukesh.24065@gmail.com>
2021-04-02 16:24:37 +0530

    salesadminlead,chainstoresalesadmin to bigint

---
+++ database/alter_customerreps_change_salesadminlead_and_chainstoresalesadmin_to_bigint_.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `customers` CHANGE `salesadminlead` `salesadminlead` BIGINT NULL DEFAULT NULL; 
+ALTER TABLE `customers` CHANGE `chainstoresalesadmin` `chainstoresalesadmin` BIGINT NULL DEFAULT NULL; 
