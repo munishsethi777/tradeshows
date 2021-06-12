39f5b46eac067d15446b2859b4019615ecb19692
Karanvir Singh <satyainfopages.karanvir@gmail.com>
2021-06-12 17:08:29 +0530

    file

---
+++ emailtemplates/RejectedQCScheduleNotificationEmailTemplate.php
@@ -0,0 +1,17 @@
+<html>
+
+<body>
+    <div style="background-color: #f3f3f4; width: 100%; color: #676a6c; font-family: open sans, Helvetica Neue, Helvetica, Arial, sans-serif">
+        <div style="background-color: white; margin: auto; max-width: 800px; padding: 0px 15px 0px 15px">
+            <div style="min-height: 700px;">
+                <div style="padding: 10px; background-color: rgba(26, 178, 147, 0.3) !important; color: white; margin: 0px -15px 0px -15px;">
+                    <div style="width:30%;display:inline-table"><img src="http://www.alpinebi.com/images/logo.png" style="width:75%;align: left"></div>
+                    <div style="width:65%;text-align:right;display:inline-table"><a href="http://www.alpinebi.com/" style="float: right; color: grey; text-decoration: none;">www.AlpineBI.com</a></div>
+                </div>
+                <h3> This emial consist of the Excel file having all the rejected QC Schedules.</h3>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
